807ee4c58e141164c42fb3a3c4ba8e41b9e8cb75
rilymanbruh <tuzturuz@gmail.com>
2024-02-17 19:52:19 +0300

    commit4
    ходьба + норм гравитация

--- Scripts/PLayerController.cs
+++ Scripts/PLayerController.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class PLayerController : MonoBehaviour
 {
     public float gravity = 9.8f;
+    public float jumpForce;
+    public float speed;
 
+    private Vector3 _moveVector;
     private float _fallVelocity = 0;
 
     private CharacterController _characterController;
@@ -16,10 +19,50 @@ public class PLayerController : MonoBehaviour
         _characterController = GetComponent<CharacterController>();
     }
 
+    void Update()
+    {
+        //Movement
+        _moveVector = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            _moveVector += transform.forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            _moveVector -= transform.forward;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            _moveVector += transform.right;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            _moveVector -= transform.right;
+        }
+
+
+        //Jump
+        if (Input.GetKeyDown(KeyCode.Space) && _characterController.isGrounded)
+        {
+            _fallVelocity = -jumpForce;
+        }
+    }
+
     // Update is called once per frame
     void FixedUpdate()
     {
+        //movement
+        _characterController.Move(_moveVector * speed * Time.fixedDeltaTime);
+
+        //fall and jump
         _fallVelocity += gravity * Time.fixedDeltaTime;
         _characterController.Move(Vector3.down * _fallVelocity * Time.fixedDeltaTime);
+
+        //stop fall if on the ground
+        if(_characterController.isGrounded)
+        {
+            _fallVelocity = 0;
+        }
     }
 }
